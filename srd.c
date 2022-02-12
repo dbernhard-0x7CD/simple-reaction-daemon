7cdd701af629825ed36a0b1e15377f92ef92dd3c
dbernhard <dbernhard@ethz.ch>
2022-02-12 22:20:05 +0100

    implement restarting of any service after specified time

--- srd.c
+++ srd.c
@@ -2,39 +2,93 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <signal.h>
-#include <time.h>
 #include <math.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <systemd/sd-bus.h>
+#include <libconfig.h>
+#include <sys/stat.h>
+#include <sys/time.h>
 
 #include "srd.h"
+
+char config_fpath[] = "/etc/srd/srd.conf";
+const char version[] = "0.0.1";
+
+#define DEBUG 1
+
+#define LOGLEVEL_DEBUG 1
+#define LOGLEVEL_INFO 0
 
 #define CLOCKID CLOCK_REALTIME
 #define SIG SIGRTMIN
 
-char IP[] = "10.10.0.11";
+typedef struct action_t {
+    const char*   name;
+    const char*   object;
+    const int     delay;
+} action_t;
 
-char version[] = "0.0.1";
-
-#define DEBUG 1
-
-// frequency of connectivity checks in ms
-#define FREQ 1000 * 60
-
-#define MAX_MS_DISCONNECTED 4000
-
-#define WAIT_FOR_REPLY_SECONDS 1
+int running = 1;
 
 void signal_handler(int s)
 {
-    // TODO
+    printf("i received signal %d\n", s);
+    fflush(stdout);
+    // TODO for shutting down, restarting
 }
 
+int restart_system() {
+    sd_bus_error error = SD_BUS_ERROR_NULL;
+    sd_bus_message *m = NULL;
+    sd_bus *bus = NULL;
+    const char *path;
+    int r;
 
-int check_connectivity()
+    /* Connect to the system bus */
+    r = sd_bus_open_system(&bus);
+    if (r < 0)
+    {
+        fprintf(stderr, "Failed to connect to system bus: %s\n", strerror(-r));
+        goto finish;
+    }
+    
+    r = sd_bus_call_method(
+        bus,
+        "org.freedesktop.systemd1",         /* service to contact */
+        "/org/freedesktop/systemd1",        /* object path */
+        "org.freedesktop.systemd1.Manager",    /* interface name */
+        "Reboot",                          /* method name */
+        &error,                             /* object to return error in */
+        &m,                                 /* return message on success */
+        "s",                                /* input signature */
+        "fail");
+    if (r < 0)
+    {
+        fprintf(stderr, "Failed to issue method call: %s\n", error.message);
+        goto finish;
+    }
+
+    /* Parse the response message */
+    r = sd_bus_message_read(m, "o", &path);
+    if (r < 0)
+    {
+        fprintf(stderr, "Failed to parse response message: %s\n", strerror(-r));
+        goto finish;
+    }
+
+    printf("Reboot scheduled. Service job: %s.\n", path);
+
+finish:
+    sd_bus_error_free(&error);
+    sd_bus_message_unref(m);
+    sd_bus_unref(bus);
+
+    return r < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
+}
+
+int check_connectivity(const char* ip, int timeout)
 {
     int pipefd[2];
     pipe(pipefd);
@@ -51,18 +105,19 @@ int check_connectivity()
 
         close(pipefd[0]);
 
-        int length = (int)((ceil(log10(WAIT_FOR_REPLY_SECONDS))+1)*sizeof(char));
+        int length = (int)((ceil(log10(1.0* timeout))+1)*sizeof(char));
         char str[length];
-        sprintf(str, "%d", WAIT_FOR_REPLY_SECONDS);
+        sprintf(str, "%d", timeout);
 
         execlp("ping", "ping", 
                     "-c", "1",
                     "-w", str,
-                    IP, (char *)0);
+                    ip, (char *)0);
     }
     else if (f < 0)
     {
         printf("forking did not work\n");
+        fflush(stdout);
         exit(1);
     }
     else
@@ -79,69 +134,185 @@ int check_connectivity()
 
         return status == 0;
     }
+    return 1;
 }
 
 int main()
 {
+    signal(SIGHUP, signal_handler);
+    signal(SIGABRT, signal_handler);
+    signal(SIGINT, signal_handler);
+    signal(SIGALRM, signal_handler);
+    signal(SIGILL, signal_handler);
+    signal(SIGKILL, signal_handler);
+
+    // load configuration
+    int count;
+    config_t cfg;
+    config_setting_t *setting;
+    config_init(&cfg);
+
+    if (access(config_fpath, F_OK)) {
+        printf("missing config file at %s", config_fpath);
+        fflush(stdout);
+        config_destroy(&cfg);
+        return EXIT_FAILURE;
+    }
+
+    if (!config_read_file(&cfg, config_fpath)) {
+        fprintf(stderr, "%s:%d - %s\n", config_error_file(&cfg), config_error_line(&cfg), config_error_text(&cfg));
+        fflush(stdout);
+        config_destroy(&cfg);
+        return EXIT_FAILURE;
+    }
+
+    const char *ip;
+    int timeout;
+    int freq;
+    const char *loglevel;
+
+    if (!config_lookup_string(&cfg, "destination", &ip)) {
+        printf("missing setting: destination\n");
+        config_destroy(&cfg);
+        return EXIT_FAILURE;
+    }
+
+    if (!config_lookup_int(&cfg, "frequency", &freq)) {
+        printf("missing setting: freq\n");
+        config_destroy(&cfg);
+        return EXIT_FAILURE;
+    }
+
+    if (!config_lookup_int(&cfg, "timeout", &timeout)) {
+        printf("missing setting: timeout\n");
+        config_destroy(&cfg);
+        return EXIT_FAILURE;
+    }
+
+    // load the actions
+    setting = config_lookup(&cfg, "actions");
+    if (setting == NULL) {
+        printf("missing actions in config file.\n");
+        config_destroy(&cfg);
+        return EXIT_FAILURE;
+    }
+    count = config_setting_length(setting);
+    printf("amount of actions: %d\n", count);
+    action_t actions[count];
+
+    for (int i = 0; i < count; i++) {
+        config_setting_t *action = config_setting_get_elem(setting, i);
+
+        const char* action_name;
+        if (!config_setting_lookup_string(action, "action", &action_name)) {
+            printf("Element is missing the action\n");
+            config_destroy(&cfg);
+            return EXIT_FAILURE;
+        }
+        actions[i].name = action_name;
+
+        if (strcmp(action_name, "reboot") == 0) {
+            if (!config_setting_lookup_int(action, "delay", &actions[i].delay)) {
+                printf("Element is missing the delay\n");
+                config_destroy(&cfg);
+                return EXIT_FAILURE;
+            }
+        } else { // must be service-restart
+            if (!config_setting_lookup_int(action, "delay", &actions[i].delay)) {
+                printf("Element is missing the delay\n");
+                config_destroy(&cfg);
+                return EXIT_FAILURE;
+            }
+            if (!config_setting_lookup_string(action, "name", &actions[i].object)) {
+                printf("Element is missing the name\n");
+                config_destroy(&cfg);
+                return EXIT_FAILURE;
+            }
+        }
+    }
+
     // data
-    int running = 1;
     int ms_since_last_reply = 0;
+
+    sd_bus *bus = NULL;
+    const char *path;
+    int r;
+
+    /* Connect to the system bus */
+    r = sd_bus_open_system(&bus);
+    if (r < 0)
+    {
+        fprintf(stderr, "Failed to connect to system bus: %s\n", strerror(-r));
+        return EXIT_FAILURE;
+    }
 
     printf("Starting srd (Simple Reconnect Daemon) version ");
     printf(version);
     printf("\n");
+    printf("Target IP: %s\n", ip);
+    printf("Frequency: %d\n", freq);
+    printf("Ping timeout: %d\n", timeout);
 
     fflush(stdout);
 
-    while (1)
-    {
-        // timer_settime(timerid, 0, &trigger, NULL);
+    struct timespec stop, start;
 
-        int connected = check_connectivity();
+    while (running)
+    {
+        clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+
+        int connected = check_connectivity(ip, timeout);
+        char *p;
+        int len;
+        time_t t = time(NULL);
+
+        p = ctime(&t);
+        len = strlen(p);
+        
         if (connected == 1)
         {
 #if DEBUG
-            char *p;
-            int len;
-            time_t t = time(NULL);
-
-            p = ctime(&t);
-            len = strlen(p);
-
             printf("still reachable %.*s\n\n\n", len -1, p);
 #endif
             ms_since_last_reply = 0;
         }
         else
         {
-            printf("disconnected \n\n");
-            ms_since_last_reply += FREQ;
+            clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
+
+            uint64_t delta_us = (stop.tv_sec - start.tv_sec) * 1000 + (stop.tv_nsec - start.tv_nsec) / 1000000;
+            ms_since_last_reply += delta_us;
+
+            printf("disconnected at %.*s; now for %dms\n\n", len -1, p, ms_since_last_reply);
         }
 
-        if (ms_since_last_reply >= MAX_MS_DISCONNECTED)
-        {
-            printf("now do reconnect action\n");
+        // check if any action is required
+        for (int i = 0; i < count; i++) {
+            if (actions[i].delay * 1000 <= ms_since_last_reply) {
+                printf("should do action: %s\n", actions[i].name);
 
-            if (0 == restart_service()) {
-                printf("dispatched command to restart\n");
-            } else {
-                printf("failed to dispatch\n");
-                exit(1);
+                if (strcmp(actions[i].name, "service-restart") == 0) {
+                    restart_service(actions[i].object);
+                } else if (strcmp(actions[i].name, "reboot")) {
+                    // restart_system(); TODO
+                }
             }
-
-            
         }
 
         fflush(stdout);
 
-        usleep(FREQ * pow(10, 3));
+        sleep(freq);
+        ms_since_last_reply += freq * 1000;
     }
 
     return 0;
 }
 
-int restart_service()
+int restart_service(char* name)
 {
+#if DEBUG
+    printf("restart_service %s\n", name);
+#endif
     sd_bus_error error = SD_BUS_ERROR_NULL;
     sd_bus_message *m = NULL;
     sd_bus *bus = NULL;
@@ -155,17 +326,24 @@ int restart_service()
         fprintf(stderr, "Failed to connect to system bus: %s\n", strerror(-r));
         goto finish;
     }
+    char service_name[256];
+    char *prefix = "/org/freedesktop/systemd1/unit/";
+    int len = strlen(prefix);
+    strcpy(service_name, prefix);
+    strcpy(service_name + len, name);
 
+    printf("object path: %s\n", service_name);
     
+
     r = sd_bus_call_method(
         bus,
-        "org.freedesktop.systemd1",         /* service to contact */
-        "/org/freedesktop/systemd1/unit/wg_2dquick_40wg0_2eservice",        /* object path */
-        "org.freedesktop.systemd1.Unit",    /* interface name */
-        "Restart",                          /* method name */
-        &error,                             /* object to return error in */
-        &m,                                 /* return message on success */
-        "s",                                /* input signature */
+        "org.freedesktop.systemd1",       /* service to contact */
+        service_name,                     /* object path */
+        "org.freedesktop.systemd1.Unit",  /* interface name */
+        "Restart",                        /* method name */
+        &error,                           /* object to return error in */
+        &m,                               /* return message on success */
+        "s",                              /* input signature */
         "fail");
     if (r < 0)
     {
--- srd.h
+++ srd.h
@@ -4,5 +4,7 @@
 
 int main();
 int restart_service();
+int restart_system();
+int check_connectivity(const char* ip, int timeout);
 
 #endif
