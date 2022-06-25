586986afc4572db0adfc988eda6974580602e3b2
dbernhard <dbernhard@ethz.ch>
2022-06-25 20:52:36 +0200

    Formatting

--- util.c
+++ util.c
@@ -89,42 +89,45 @@ int ends_with(char* str, char* end) {
 
 // this is from: https://gist.github.com/bg5sbk/11058000
 char* str_replace(char* string, const char* substr, const char* replacement) {
-	char* tok = NULL;
-	char* newstr = NULL;
-	char* oldstr = NULL;
-	int oldstr_len = 0;
-	int substr_len = 0;
-	int replacement_len = 0;
+    char* tok = NULL;
+    char *newstr = NULL;
+    char *oldstr = NULL;
+    int oldstr_len = 0;
+    int substr_len = 0;
+    int replacement_len = 0;
 
-	newstr = strdup(string);
-	substr_len = strlen(substr);
-	replacement_len = strlen(replacement);
+    newstr = strdup(string);
+    substr_len = strlen(substr);
+    replacement_len = strlen(replacement);
 
-	if (substr == NULL || replacement == NULL) {
-		return newstr;
-	}
+    free(string);
 
-	while ((tok = strstr(newstr, substr))) {
-		oldstr = newstr;
-		oldstr_len = strlen(oldstr);
-		newstr = (char*)malloc(sizeof(char) * (oldstr_len - substr_len + replacement_len + 1));
+    if (substr == NULL || replacement == NULL)
+    {
+        return newstr;
+    }
 
-		if (newstr == NULL) {
-			free(oldstr);
-			return NULL;
-		}
+    while ((tok = strstr(newstr, substr)))
+    {
+        oldstr = newstr;
+        oldstr_len = strlen(oldstr);
+        newstr = (char *)malloc(sizeof(char) * (oldstr_len - substr_len + replacement_len + 1));
 
-		memcpy(newstr, oldstr, tok - oldstr);
-		memcpy(newstr + (tok - oldstr), replacement, replacement_len);
-		memcpy(newstr + (tok - oldstr) + replacement_len, tok + substr_len, oldstr_len - substr_len - (tok - oldstr));
-		memset(newstr + oldstr_len - substr_len + replacement_len, 0, 1);
+        if (newstr == NULL)
+        {
+            free(oldstr);
+            return NULL;
+        }
 
-		free(oldstr);
-	}
+        memcpy(newstr, oldstr, tok - oldstr);
+        memcpy(newstr + (tok - oldstr), replacement, replacement_len);
+        memcpy(newstr + (tok - oldstr) + replacement_len, tok + substr_len, oldstr_len - substr_len - (tok - oldstr));
+        memset(newstr + oldstr_len - substr_len + replacement_len, 0, 1);
 
-	free(string);
+        free(oldstr);
+    }
 
-	return newstr;
+    return newstr;
 }
 
 char* get_default_gw() {
