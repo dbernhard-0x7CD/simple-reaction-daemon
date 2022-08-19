f5135ced722b2459a6584b925861f88d15df57e1
dbernhard <dbernhard@ethz.ch>
2022-08-19 21:27:50 +0200

    Increment version for next release

--- srd.c
+++ srd.c
@@ -21,7 +21,7 @@
 
 char *const configd_path = "/etc/srd/";
 char *const config_main = "/srd.conf";
-char *const version = "0.0.6";
+char *const version = "0.0.7-dev";
 
 // application configuration
 enum loglevel loglevel = LOGLEVEL_DEBUG;
